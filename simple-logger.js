40bd0d85eb0cf4aa7ee3ff568ed0753e10d11f82
Jon Gretar <jongretar@jongretar.com>
2015-02-13 22:15:41 +0000

    Direct some log levels to stderr

--- simple-logger.js
+++ simple-logger.js
@@ -19,8 +19,8 @@ SimpleLogger.prototype.should = function (level) {
   return levels[level] >= levels[this.level];
 };
 
-SimpleLogger.prototype.fatal = function (message) { if (this.should('fatal')) { console.log('[FATAL] '+message) } };
-SimpleLogger.prototype.error = function (message) { if (this.should('error')) { console.log('[ERROR] '+message) } };
+SimpleLogger.prototype.fatal = function (message) { if (this.should('fatal')) { console.error('[FATAL] '+message) } };
+SimpleLogger.prototype.error = function (message) { if (this.should('error')) { console.error('[ERROR] '+message) } };
 SimpleLogger.prototype.warn = function (message) { if (this.should('warn')) { console.log('[WARN] '+message) } };
 SimpleLogger.prototype.info = function (message) { if (this.should('info')) { console.log('[INFO] '+message) } };
 SimpleLogger.prototype.debug = function (message) { if (this.should('debug')) { console.log('[DEBUG] '+message) } };
