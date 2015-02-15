cff5cdf8ac254b8624a8396a14f895cb0d3d12a4
Jon Gretar <jongretar@jongretar.com>
2015-02-15 12:06:44 +0000

    Do some cover ignores on SimpleLogger

--- simple-logger.js
+++ simple-logger.js
@@ -12,6 +12,7 @@ var levels = {
 module.exports = SimpleLogger;
 
 function SimpleLogger() {
+  /* istanbul ignore next  */
   this.level = process.env.LOG_LEVEL || 'debug';
 }
 
@@ -19,9 +20,12 @@ SimpleLogger.prototype.should = function (level) {
   return levels[level] >= levels[this.level];
 };
 
-SimpleLogger.prototype.fatal = function (message) { if (this.should('fatal')) { console.error('[FATAL] '+message) } };
-SimpleLogger.prototype.error = function (message) { if (this.should('error')) { console.error('[ERROR] '+message) } };
-SimpleLogger.prototype.warn = function (message) { if (this.should('warn')) { console.log('[WARN] '+message) } };
-SimpleLogger.prototype.info = function (message) { if (this.should('info')) { console.log('[INFO] '+message) } };
-SimpleLogger.prototype.debug = function (message) { if (this.should('debug')) { console.log('[DEBUG] '+message) } };
-SimpleLogger.prototype.trace = function (message) { if (this.should('trace')) { console.log('[TRACE] '+message) } };
+/* istanbul ignore next  */
+(function () {
+  SimpleLogger.prototype.fatal = function (message) { if (this.should('fatal')) { console.error('[FATAL] '+message) } };
+  SimpleLogger.prototype.error = function (message) { if (this.should('error')) { console.error('[ERROR] '+message) } };
+  SimpleLogger.prototype.warn = function (message) { if (this.should('warn')) { console.log('[WARN] '+message) } };
+  SimpleLogger.prototype.info = function (message) { if (this.should('info')) { console.log('[INFO] '+message) } };
+  SimpleLogger.prototype.debug = function (message) { if (this.should('debug')) { console.log('[DEBUG] '+message) } };
+  SimpleLogger.prototype.trace = function (message) { if (this.should('trace')) { console.log('[TRACE] '+message) } };
+})();
